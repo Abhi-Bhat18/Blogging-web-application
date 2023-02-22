4578711ba6efb0c85146b3bc98910e5db39dbe7c
Abhi-Bhat18 <abhishekbhat344@gmail.com>
2023-02-22 12:53:34 +0530

    render allowed

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://hardware-techy-blog.onrender.com/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
